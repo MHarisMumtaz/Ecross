04fa77ea8ba95cccd5f47969cf554cb8cf436591
Haris Mumtaz <>
2020-08-07 16:46:23 +0500

    added events registration

--- src/navigations/RegisterEvents.js
+++ src/navigations/RegisterEvents.js
@@ -1,9 +1,10 @@
 import { Navigation } from 'react-native-navigation';
+import { BOTTOM_TAB_STACK_ID, HAMBURGER_BUTTON_ID } from '../utils/Constants';
 
 const registerEvents = () => {
 	Navigation.events().registerNavigationButtonPressedListener(({ buttonId }) => {
-		if(buttonId === 'sideMenu'){
-			Navigation.mergeOptions('sideMenuCenterStack', {
+		if(buttonId === HAMBURGER_BUTTON_ID){
+			Navigation.mergeOptions(BOTTOM_TAB_STACK_ID, {
 		        sideMenu: {
 		          left: {
 		            visible: true
