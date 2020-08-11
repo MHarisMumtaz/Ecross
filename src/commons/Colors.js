ac9e1453bdb018dcd856fbdc5d6c13bfe89431cd
Haris Mumtaz <>
2020-08-11 15:42:35 +0500

    added color collections

--- src/commons/Colors.js
+++ src/commons/Colors.js
@@ -5,4 +5,21 @@ const colors = {
 	grey: 'grey',
 	white: 'white'
 }
+const ColorsCollection = [
+	'#B6EA7D',
+	'#E3B9FF',
+	'#ff5d5a',
+	'#BFD3F9',
+	'#FFC34D',
+	'#C2BBEA',
+	'#FF6A6A',
+	'#9AD79A',
+	'#DDB7DF',
+	'#E9FBA6',
+	'#E5F1FF',
+	'#9AD79A',
+	'#E5E5F7',
+	'#F67FBF'
+]
+export { ColorsCollection };
 export default colors;
