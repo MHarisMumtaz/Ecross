a492500d0b759f761fb24cec3666c0096baf13a2
Haris Mumtaz <>
2020-08-14 18:33:58 +0500

    initiated Product Detail

---
+++ src/screens/ProductDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Image, ImageBackground, View, Text, Dimensions } from 'react-native';
+import colors from '../commons/Colors';
+
+const ProductDetails = ({}) => {
+    return (
+      <View>
+        <Text>Product Details</Text>
+      </View>
+    );
+}
+
+export default ProductDetails;
+const styles = StyleSheet.create({
+ 
+});
