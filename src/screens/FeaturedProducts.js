166f8e6eba5a63dfcbddf7e8aeda1694fc714cad
Haris Mumtaz <>
2020-08-14 18:35:43 +0500

    added see all label on featured list

--- src/screens/FeaturedProducts.js
+++ src/screens/FeaturedProducts.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, FlatList, View, Text, Dimensions } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList, View, Text, TouchableOpacity, Dimensions } from 'react-native';
 import ProductCard from './ProductCard';
+import colors from '../commons/Colors';
 
 const DATA = [
 	{
 		id: '21',
-		title: 'Mens TShirt',
+		title: 'Mens TShirt all for dress split',
 		image: 'https://purepng.com/public/uploads/medium/purepng.com-men-t-shirtclothingmen-t-shirtfashion-dress-shirt-cloth-tshirt-631522326839xqywo.png',
 		price: '$400.0',
 		totalRating: '2'
@@ -26,8 +27,12 @@ const DATA = [
 	}
 ];
 
-const FeaturedProducts = () => {
-    
+const FeaturedProducts = (props) => {
+	
+	const onPressSeeAll = () => {
+
+	}   
+
     const renderItem = ({item, index}) => {
     	return (<View key={index} style={styles.productBox}>
     				<ProductCard
@@ -42,9 +47,14 @@ const FeaturedProducts = () => {
 
     return (
     	<SafeAreaView>
-    		<Text style={styles.featuredLabel}>
-    			Featured
-    		</Text>
+    		<View style={styles.labelSection}>
+	    		<Text style={styles.featuredLabel}>
+	    			Featured
+	    		</Text>
+	    		<TouchableOpacity style={styles.seeAllBtn} onPress={onPressSeeAll}>
+	    			<Text style={styles.seeAllBtnText}>See all</Text>
+	    		</TouchableOpacity>
+    		</View>
 	    	<FlatList
 	    	 data={DATA}
 	    	 renderItem={renderItem}
@@ -66,5 +76,16 @@ const styles = StyleSheet.create({
 	  	paddingLeft: 20,
 	  	fontSize: 20,
 	  	fontWeight:'bold'
+	},
+	seeAllBtn: {
+		paddingRight:20,
+	},
+	seeAllBtnText: {
+		fontSize:15,
+		color: colors.primary
+	},
+	labelSection: {
+		flexDirection: 'row',
+		justifyContent: 'space-between'
 	}
 })
