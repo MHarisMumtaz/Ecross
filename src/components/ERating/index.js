39d9c9ec6d13ebb11be537c0053674ce9f119621
Haris Mumtaz <>
2020-08-31 17:42:51 +0500

    Created rating component

---
+++ src/components/ERating/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View, Text, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import EIconButton from '../EIconButton';
+
+import colors from '../../commons/Colors';
+
+const ERating = ({rating, filledIcon = 'md-star', filledColor = colors.golden, blankIcon = 'md-star-outline', halfIcon = 'md-star-half-sharp' , blankColor = colors.golden, maxStars = 5, size = 20, containerStyle, onPress = () => {}}) => {
+
+	const renderStars = () => {
+		const stars = [];
+		let blankStarsCount = Math.round(rating * 2) / 2;
+
+		for(let i = 0; i < maxStars; i++){
+			let iconColor = filledColor;
+			let iconName = filledIcon;
+			if(blankStarsCount>0 && blankStarsCount<1){
+				iconName = halfIcon;
+			}else if(blankStarsCount<=0){
+				iconName = blankIcon;
+				iconColor = blankColor;
+			}
+		   	stars.push(
+			   	<TouchableOpacity key={i} onPress={()=> onPress(i)}>
+					 <Icon name={iconName} size={size} color={iconColor} />
+				</TouchableOpacity>
+			);
+			blankStarsCount = blankStarsCount - 1;
+		}
+		return stars;
+	}
+
+    return (
+        <View style={[styles.container,containerStyle]}>
+            {renderStars()}
+        </View>
+    );
+}
+
+export default ERating;
+const styles = StyleSheet.create({
+	container:{
+		flexDirection: 'row'
+	}
+});
--- src/screens/ProductDetails.js
+++ src/screens/ProductDetails.js
@@ -3,6 +3,7 @@ import { StyleSheet, TouchableOpacity, SafeAreaView, ScrollView, Image, ImageBac
 import colors, { ColorsCollection } from '../commons/Colors';
 import ECarousal from '../components/ECarousal';
 import ESelectableColorList from '../components/ESelectableColorList';
+import ERating from '../components/ERating';
 
 const data = [
 	{
@@ -42,14 +43,30 @@ const ProductDetails = ({}) => {
 	const [colorCollection,setColorCollection] = useState([...productColors]);
 
 	const onColorPress = (item, i) => {
-		colorCollection[i].selected =! colorCollection[i].selected;
-		setColorCollection([...colorCollection]);
+		const updatedCollection = colorCollection.map((obj,index)=>{
+			obj.selected = false;
+			if(index===i){
+				obj.selected =! obj.selected;
+			}
+			return obj;
+		});
+		setColorCollection(updatedCollection);
 	};
 
     return (
       <SafeAreaView>
       	<ScrollView nestedScrollEnabled={true}>
         	<ECarousal items={data} />
+        	<View style={styles.body}>
+	        	<View style={styles.titleSection}>
+	        		<Text style={styles.productTitle}>Mens T-Shirt </Text>
+		        	<View>
+			        	<Text style={styles.productOldPrice} > $400.00</Text>
+			        	<Text style={styles.productPrice}> $500.00 </Text>
+		        	</View>
+	        	</View>
+		        <ERating rating={1.4} />
+	        </View>
         	<ESelectableColorList colorsCollection={colorCollection} onPress={(item,i)=> onColorPress(item,i)} />
         </ScrollView>
       </SafeAreaView>
@@ -58,4 +75,27 @@ const ProductDetails = ({}) => {
 
 export default ProductDetails;
 const styles = StyleSheet.create({
+	body:{
+		padding: 10,
+	},
+	titleSection: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	productTitle:{
+		fontSize:22,
+		fontWeight: 'bold',
+	},
+	productPrice: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		color: colors.primary
+	},
+	productOldPrice: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		textDecorationLine: 'line-through', 
+		textDecorationStyle: 'solid',
+		color: colors.secondary
+	}
 });
