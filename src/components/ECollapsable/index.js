90e2fa14bde88183264a802da9d270b381cc11f1
Haris Mumtaz <>
2020-09-05 19:39:13 +0500

    added collapsable component

---
+++ src/components/ECollapsable/index.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react';
+import { Platform, SafeAreaView, StyleSheet, TouchableOpacity, Pressable, FlatList, View, Text, LayoutAnimation, UIManager } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import colors from '../../commons/Colors';
+
+const ECollapsable = ({containerStyle, nonExpandedIcon = 'arrow-forward-outline', expandedIcon = 'arrow-down-outline', expandedIconColor=colors.primary, name, icon, iconSize = 20, iconColor = colors.primary, data, childComponent}) => {
+
+  const [expanded, setExpanded] = useState(false);
+  if (Platform.OS === 'android') {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+
+  const toggleExpand = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setExpanded(!expanded);
+  }
+
+  return (
+     <View style={[styles.container]}>
+        <TouchableOpacity style={[styles.row,containerStyle]} onPress={()=>toggleExpand()}>
+            {icon && <Icon name={icon} size={iconSize} color={iconColor} />}
+            <Text style={[styles.title, styles.font]}>{name}</Text>
+            { (data && data.length>0) && <Icon name={expanded ? expandedIcon : nonExpandedIcon} size={20} color={expandedIconColor} />}
+        </TouchableOpacity>
+        {!expanded && <View style={styles.parentHr}/>}
+        {
+          expanded &&
+         <View style={{}}>
+           {
+             data.map((item,index)=> 
+               (childComponent ? <childComponent key={index} item={item} /> :
+                <View key={index}>
+                    <TouchableOpacity style={[styles.childRow]} onPress={()=>this.onClick(index)}>
+                        {item.icon && <Icon name={item.icon} size={item.iconSize} color={item.iconColor} />}
+                        <Text style={[styles.childText]} >{item.name}</Text>
+                    </TouchableOpacity>
+                    <View style={styles.childHr}/>
+                </View>))
+           }
+          </View>
+        }
+     </View>
+  )
+}
+
+
+export default ECollapsable;
+
+const styles = StyleSheet.create({
+  container:{
+  },
+  title:{
+    fontSize: 14,
+    fontWeight:'bold',
+    color: colors.primary,
+  },
+  childText:{
+    color: colors.white,
+    fontSize: 16
+  },
+  row:{
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    height:56,
+    paddingLeft:25,
+    paddingRight:18,
+    alignItems:'center',
+    backgroundColor: colors.grey,
+  },
+  parentHr:{
+    height:1,
+    color: colors.white,
+    width:'100%'
+  },
+    title:{
+        fontSize: 14,
+        fontWeight:'bold',
+        color: colors.grey,
+    },
+    itemActive:{
+        fontSize: 12,
+        color: colors.primary,
+    },
+    btnActive:{
+        borderColor: colors.primary,
+    },
+    btnInActive:{
+        borderColor: colors.grey,
+    },
+    row:{
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        height:56,
+        paddingLeft:25,
+        paddingRight:18,
+        alignItems:'center',
+        backgroundColor: colors.lightGrey,
+    },
+    childRow:{
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        backgroundColor: colors.lightGrey,
+        borderBottomColor: colors.white,
+        borderBottomWidth: 1,
+        width:'100%',
+        height:45,
+        alignItems:'center',
+        paddingLeft:45,
+    },
+    parentHr:{
+        height:1,
+        color: colors.white,
+        width:'100%'
+    },
+    childHr:{
+        height:1,
+        backgroundColor: colors.lightGrey,
+        width:'100%',
+    },
+    colorActive:{
+        borderColor: colors.primary,
+    },
+    colorInActive:{
+        borderColor: colors.grey,
+    }
+});
--- src/screens/LeftSideMenu.js
+++ src/screens/LeftSideMenu.js
@@ -1,9 +1,72 @@
 import React, { useEffect, useState } from 'react';
-import { Text, SafeAreaView, ScrollView, StyleSheet, Button } from 'react-native';
+import { Text, SafeAreaView, ScrollView, StyleSheet, View, Image, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import colors from '../commons/Colors';
+import ETextButton from '../components/ETextButton';
+import ECollapsable from '../components/ECollapsable';
+
+const categories = [
+  {
+    id: 1,
+    name: 'Mens',
+    parent: 0
+  },
+  {
+    id: 1,
+    name: 'T-Shirts',
+    parent: 1
+  },
+  {
+    id: 1,
+    name: 'Pants',
+    parent: 1
+  },
+  {
+    id: 1,
+    name: 'Caps',
+    parent: 1
+  }
+]
+
+const sideMenuData = [
+  {
+    id: 1,
+    title: 'Home',
+    icon: 'home-outline',
+    iconSize: 18,
+    iconColor: colors.primary
+  },
+  {
+    id: 2,
+    title: 'Categories',
+    data: categories
+  },
+  {
+    id: 3,
+    title: 'About us',
+    icon: 'alert-circle-outline',
+    iconSize: 18,
+    iconColor: colors.primary
+  },
+  {
+    id: 4,
+    title: 'Contact us',
+    icon: 'call-outline',
+    iconSize: 18,
+    iconColor: colors.primary
+  },
+  {
+    id: 5,
+    title: 'Notifications',
+    icon: 'notifications-outline',
+    iconSize: 18,
+    iconColor: colors.primary
+  }
+]
 
 const LeftSideMenu = () => {
 
-  const pushAndClose = () =>
+  const pushAndClose = () => {
     Navigation.push('SideMenuCenter', {
       component: {
         name: Screens.Pushed,
@@ -16,13 +79,53 @@ const LeftSideMenu = () => {
         },
       },
     });
+  }
+
+  const renderSingleOption = (item) => {
+    return (
+      <TouchableOpacity key={item.id} style={styles.singleOption} onPress={() => {}}>
+        {item.icon && <Icon name={item.icon} size={item.iconSize} color={item.iconColor} />}
+        <Text style={styles.title}>{item.title}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  const getSubCategories = (data,parentId) => {
+    return data.filter((item)=> item.parent != parentId);
+  }
+
+  const renderCategories = (data) => {
+    return data.map((cat, index)=> {
+        if(cat.parent === 0){
+          const subCategories = getSubCategories(data, cat.parent);
+          return <ECollapsable key={index} containerStyle={styles.parentCatgeory} name={cat.name} data={subCategories} />
+        }
+    });
+  }
+
+  const renderMenuOptions = () => {
+    return sideMenuData.map((item, i)=>!item.data ? renderSingleOption(item) : renderCategories(item.data))
+  }
 
   return (
     <SafeAreaView style={styles.root}>
-      <ScrollView contentContainerStyle={[styles.scrollView]}>
-        
-          <Text>Push</Text>
-        
+      <ScrollView contentContainerStyle={[styles.scrollView]} nestedScrollEnabled={true}>
+          <View style={styles.header}>
+            <View style={styles.profileSection}>
+               <Image 
+                style={styles.profileImage}
+                source={{uri: 'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'}}
+               />
+               <View style={styles.userDetail}>
+                 <Text style={styles.userName}>Guest</Text>
+                 <Text style={styles.email}>demo@gmail.com</Text>
+               </View>
+            </View>
+             <ETextButton containerStyle={styles.loginButton}>
+               <Text style={styles.loginText}>Login <Icon name='arrow-forward-outline' size={16} /></Text>
+             </ETextButton>
+          </View>
+          { renderMenuOptions() }
       </ScrollView>
     </SafeAreaView>
   );
@@ -30,13 +133,65 @@ const LeftSideMenu = () => {
 export default LeftSideMenu;
 
 const styles = StyleSheet.create({
+  parentCatgeory:{
+    backgroundColor: colors.white
+  },
+  singleOption:{
+    flexDirection: 'row',
+    height: 56,
+    paddingLeft:25,
+    alignItems:'center',
+    backgroundColor: colors.white,
+  },
+  title:{
+    fontSize: 16,
+    fontWeight:'bold',
+    color: colors.grey,
+    paddingLeft: 10
+  },
   root: {
     flex: 1,
     backgroundColor:'white'
   },
   scrollView: {
-    flexGrow: 1,
-    alignItems: 'center',
-    padding: 16,
+    flexGrow: 1
   },
+  header:{
+    backgroundColor: colors.primary,
+    padding: 10,
+    alignItems: 'center',
+  },
+  profileSection:{
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 60,
+    borderWidth: 2,
+    
+    borderColor: colors.white
+  },
+  userName: {
+    color: colors.white,
+    fontSize: 22
+  },
+  userDetail:{
+    paddingLeft: 10
+  },
+  email:{
+    fontSize: 16,
+    color: colors.white
+  },
+  loginButton:{
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    alignSelf: 'flex-end'
+  },
+  loginText:{
+    textAlign:'center',
+    fontSize: 16,
+    color: colors.secondary
+  }
 });
