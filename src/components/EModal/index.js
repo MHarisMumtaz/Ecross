58781c216e5b965cd06deab302cca80e4f89e559
Haris Mumtaz <>
2020-09-06 14:50:07 +0500

    created community modal component

---
+++ src/components/EModal/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Modal from 'react-native-modal';
+import { Platform, Dimensions, StyleSheet, Text, Pressable, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import colors from '../../commons/Colors';
+
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+
+const EModal = ({isShow, onBackdropPress, containerStyle, children }) => {
+
+  return (
+	  <Modal
+	  	isVisible={isShow} 
+	  	onBackdropPress={onBackdropPress}
+	  	deviceWidth={windowWidth}
+      	deviceHeight={windowHeight}>
+	    <View style={[styles.container, containerStyle]}>
+	      {children}
+	    </View>
+	  </Modal>
+  );
+}
+
+
+export default EModal;
+
+const styles = StyleSheet.create({
+  container: {
+  	backgroundColor: colors.white,
+  	height: 200
+  }
+});
