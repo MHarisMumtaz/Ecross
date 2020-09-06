b31bcf68e6320f4a6f4b6db8fb0f2e7689f38b30
Haris Mumtaz <>
2020-09-06 15:47:26 +0500

    added min and max slider for price filter

--- src/screens/SearchFilters.js
+++ src/screens/SearchFilters.js
@@ -4,15 +4,55 @@ import Slider from '@react-native-community/slider';
 import colors from '../commons/Colors';
 
 const SearchFilters = () => {
+	const priceRange = {
+		minValue: 10,
+		maxValue: 10000
+	};
+
+	const [seletecPriceRange,setSelectedPriceRange] = useState({min:10, max:10000});
+
+	const onMinPriceChange = (val) => {
+		setSelectedPriceRange({...seletecPriceRange,min: val});
+	}
+
+	const onMaxPriceChange = (val) => {
+		if(seletecPriceRange.min < val)
+			setSelectedPriceRange({...seletecPriceRange, max: val});	
+	}
+
     return (
 		<View style={styles.container}>
-			<Slider
-				style={{width: 200, height: 40}}
-				minimumValue={0}
-				maximumValue={1}
-				minimumTrackTintColor="#FFFFFF"
-				maximumTrackTintColor="#000000"
-			/>
+			<View style={styles.priceSection}>
+				<Text style={styles.priceLabel}>Price: <Text style={styles.priceRange}>$0 - $10000</Text></Text>
+				<View style={styles.sliderSection}>
+					<Text style={styles.minLabel}>Min: <Text style={styles.priceRange}>${seletecPriceRange.min}</Text></Text>
+					<Slider
+						style={{width: 220, height: 40}}
+						minimumValue={priceRange.minValue}
+						maximumValue={priceRange.maxValue}
+						minimumTrackTintColor={colors.primary}
+						maximumTrackTintColor={colors.grey}
+						thumbTintColor={colors.primary}
+						onValueChange={onMinPriceChange}
+						value={seletecPriceRange.min}
+						step={5}
+					/>
+				</View>
+				<View style={styles.sliderSection}>
+					<Text style={styles.minLabel}>Max: <Text style={styles.priceRange}>${seletecPriceRange.max}</Text></Text>
+					<Slider
+						style={{width: 220, height: 40}}
+						minimumValue={priceRange.minValue}
+						maximumValue={priceRange.maxValue}
+						minimumTrackTintColor={colors.primary}
+						maximumTrackTintColor={colors.grey}
+						thumbTintColor={colors.primary}
+						onValueChange={onMaxPriceChange}
+						value={seletecPriceRange.max}
+						step={5}
+					/>
+				</View>
+			</View>
 		</View>
     );
 }
@@ -21,7 +61,24 @@ export default SearchFilters;
 
 const styles = StyleSheet.create({
 	container:{
-		padding: 10,
+		padding: 20,
 		paddingBottom: 0,
+	},
+	priceSection:{
+
+	},
+	priceLabel: {
+		fontSize: 16,
+		fontWeight: 'bold'
+	},
+	priceRange:{
+		color: colors.grey
+	},
+	minLabel: {
+		top: 7,
+		fontWeight: 'bold'
+	},
+	sliderSection:{
+		flexDirection: 'row'
 	}
 });
