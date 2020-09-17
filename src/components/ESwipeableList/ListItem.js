c3440a700cfbd87c08b693c9773d96ff7a4a438f
Haris Mumtaz <>
2020-09-17 16:09:17 +0500

    created swipeable list component

---
+++ src/components/ESwipeableList/ListItem.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, TouchableOpacity, View, Text, Animated, PanResponder, Dimensions, Platform } from 'react-native';
+import colors from '../../commons/Colors';
+
+const { width } = Dimensions.get('window');
+
+const ListItem = ({children, setScrollEnabled, success, id}) => {
+
+	const gestureDelay = 35;
+	let scrollViewEnabled = true;
+	const position = new Animated.ValueXY();
+
+	const panResponder = PanResponder.create({
+		onStartShouldSetPanResponder: (evt, gestureState) => true,
+		onMoveShouldSetPanResponder: (evt, gestureState) => true,
+		onPanResponderTerminationRequest: (evt, gestureState) => false,
+		onPanResponderMove: (evt, gestureState) => {
+		  if (gestureState.dx < -35) {
+		    setScrollViewEnabled(false);
+		    let newX = gestureState.dx - gestureDelay;
+		    position.setValue({ x: newX, y: 0 });
+		  }
+		},
+		onPanResponderRelease: (evt, gestureState) => {
+		  	if (gestureState.dx < -150) {
+			    Animated.timing(position, {
+			      toValue: {x: width, y: 0},
+			      duration: 150,
+			      useNativeDriver: false
+			    }).start(() => {
+			      success(id);
+			      setScrollViewEnabled(true);
+			    });
+		    } else {
+			    Animated.timing(position, {
+			      toValue: {x: 0, y: 0},
+			      duration: 500,
+			      useNativeDriver: false
+			    }).start(() => {
+			      setScrollViewEnabled(true);
+			    });
+		    }
+		},
+	});
+
+	const setScrollViewEnabled = (enabled) => {
+	    if (scrollViewEnabled !== enabled) {
+	      setScrollEnabled(enabled);
+	      scrollViewEnabled = enabled;
+	    }
+	  }
+
+  	return (
+	    <View style={styles.listItem}>
+	      <Animated.View 
+	        style={[position.getLayout()]} 
+	        {...panResponder.panHandlers}
+	      >
+	        <View style={styles.absoluteCell}>
+	          <Text style={styles.absoluteCellText}>DELETE</Text>
+	        </View>
+	        <View style={styles.innerCell}>
+	          { children }
+	        </View>
+	      </Animated.View>
+	    </View>
+  	);
+}
+
+export default ListItem;
+const styles = StyleSheet.create({
+  listItem: {
+    justifyContent: 'center',
+    backgroundColor: colors.delete,
+  },
+  absoluteCell: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    width: 100,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  absoluteCellText: {
+    margin: 16,
+  },
+  innerCell: {
+    width: width,
+    backgroundColor: colors.white,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ src/components/ESwipeableList/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, TouchableOpacity, FlatList, View, Text, Dimensions } from 'react-native';
+import colors from '../../commons/Colors';
+import ListItem from './ListItem';
+
+const ESwipeableList = ({containerStyle, listStyle, data = [], renderListItem, renderSeparator, onItemSwiped}) => {
+
+    const [enableScroll, setEnableScroll] = useState(true);
+
+    const renderItem = ({item, index}) => {
+        return (
+          <ListItem
+            key={index}
+            id={item.id}
+            success={onItemSwiped}
+            setScrollEnabled={enable => setEnableScroll(enable)}
+          >
+          { renderListItem(item,index) }
+          </ListItem>
+        );
+    }
+
+    const renderDefaultSeparator = () => {
+        return (
+          <View style={styles.separatorViewStyle}>
+            <View style={styles.separatorStyle} />
+          </View>
+        );
+      }
+
+    return (
+        <SafeAreaView style={containerStyle ? containerStyle : styles.container}>
+            <FlatList
+              style={listStyle}
+              data={data}
+              ItemSeparatorComponent={renderSeparator ? renderSeparator : renderDefaultSeparator}
+              renderItem={renderItem}
+              scrollEnabled={enableScroll}
+              showsVerticalScrollIndicator={false}
+            />    
+        </SafeAreaView>
+    );
+}
+
+export default ESwipeableList;
+const styles = StyleSheet.create({
+	container:{
+	},
+    separatorViewStyle: {
+        flex: 1,
+        backgroundColor: colors.lightGrey
+    },
+    separatorStyle: {
+        height: 1,
+        backgroundColor: colors.lightGrey
+    },
+});
