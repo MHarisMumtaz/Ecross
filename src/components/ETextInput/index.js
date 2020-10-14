3a489960f0d91fb40480ba48b0ca16c37c6a33b4
MHarisMumtaz <harismumtaz.pafkiet@hotmail.com>
2020-10-14 17:05:27 +0500

    updated textInput styles to remove flex issue

--- src/components/ETextInput/index.js
+++ src/components/ETextInput/index.js
@@ -7,8 +7,9 @@ const windowWidth = Dimensions.get('window').width;
 
 const ETextInput = ({
     value,
+    multiline = false,
     disabled = false,
-    inputStyle, 
+    inputStyle,
     leftIcon, 
     leftIconSize = 18, 
     leftIconColor = colors.primary, 
@@ -22,13 +23,43 @@ const ETextInput = ({
     rounded,
     shadowColor = colors.grey,
     showClearOnEditing,
+    textContentType,
     onTextChange = () => {},
     onPressClear = () => {},
     onEndEditing = () => {}
   }) => {
 
-  return (
-     <View style={[styles.container, containerStyle, showShadow && {
+  const multiLineInput = () => {
+    return (
+      <View style={[styles.textAreaContainer, containerStyle, showShadow && {
+        elevation: 4,
+        shadowColor: shadowColor ? shadowColor : colors.grey, // IOS
+        shadowOffset: { height: 1, width: 1 }, // IOS
+        shadowOpacity: 0.3, // IOS
+        shadowRadius: 3, //IOS
+       }, rounded && {
+         borderRadius: 30,
+       }]}>
+        <TextInput 
+          multiline={true}
+          numberOfLines={5}
+          style={[styles.textArea, inputStyle]}
+          textContentType={textContentType}
+          editable={!disabled}
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor={placeholderTextColor}
+          underlineColorAndroid="transparent"
+          onChangeText={onTextChange}
+          onEndEditing={onEndEditing}
+        />
+      </View>
+    )
+  }
+
+  const singleInput = () => {
+    return (
+      <View style={[styles.container, containerStyle, showShadow && {
         elevation: 4,
         shadowColor: shadowColor ? shadowColor : colors.grey, // IOS
         shadowOffset: { height: 1, width: 1 }, // IOS
@@ -51,7 +82,10 @@ const ETextInput = ({
       {(showClearOnEditing && value) && <EIconButton icon='close-outline' onPress={onPressClear} />}
       {rightIcon && <Icon style={styles.rightIcon} name={rightIcon} size={rightIconSize} color={rightIconColor} />}
      </View>
-  )
+    );
+  }
+
+  return multiline ? multiLineInput() : singleInput();
 }
 export default ETextInput;
 
@@ -60,7 +94,7 @@ const styles = StyleSheet.create({
     width: windowWidth / 1.25,
     flexDirection: 'row',
     backgroundColor: colors.white,
-    height: 40,
+    height: 45,
   },
   input:{
     flex: 1,
@@ -70,11 +104,24 @@ const styles = StyleSheet.create({
     color: colors.grey
   },
   leftIcon:{
-    paddingTop:10,
+    paddingTop:12,
     paddingLeft:10
   },
   rightIcon:{
     paddingTop: 10,
     paddingRight: 8
+  },
+  textArea:{
+    height: 85,
+    color: colors.grey,
+    textAlignVertical: 'top'
+  },
+  textAreaContainer:{
+    width: windowWidth / 1.25,
+    height: 100, 
+    padding: 10, 
+    borderRadius: 16,
+    width: 320,
+    backgroundColor: colors.white
   }
 });
